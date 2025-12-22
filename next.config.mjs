63b49b623e477423a05d3281d2e8a7df9cd46768
Arun pandian <arunpandian972000@gmail.com>
2025-12-22 18:05:15 +0530

    deploy v-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+let supabaseHost = "";
+try {
+  supabaseHost = new URL(supabaseUrl).hostname; // e.g. bjudxntmpfpbyloibloc.supabase.co
+} catch {}
 
-export default nextConfig;
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: supabaseHost,
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+  typescript: {
+    // ❗️Allows production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // (Optional) ignore ESLint errors during build too
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
